6984cdd8ef88622b8b5e8f210d6c1618cb83d742
Ivan Fisher <87035839+Ivanfisher@users.noreply.github.com>
2021-09-08 15:14:43 -0400

    Added comments

--- BasicC#Programs/IncomeComparisonProgram/IncomeComparisonProgram/Program.cs
+++ BasicC#Programs/IncomeComparisonProgram/IncomeComparisonProgram/Program.cs
@@ -11,19 +11,24 @@ namespace IncomeComparisonProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Anonymous Income Comparison Program");
+            // Asking for and storing info for Person 1 on variables
             Console.WriteLine("Please enter the following details for person 1,");
             Console.WriteLine("Hourly rate: ");
             double p1HourlyRate = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Hours worked per week: ");
             double p1HoursWorked = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Please enter the following details for person 1,");
+            // Asking for and storing info for Person 2 in variables
+            Console.WriteLine("Please enter the following details for person 2,");
             Console.WriteLine("Hourly rate: ");
             double p2HourlyRate = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Hours worked per week: ");
             double p2HoursWorked = Convert.ToDouble(Console.ReadLine());
+            // Finding yearly income for Person 1 and Person 2
             double p1AnnualSalary = p1HourlyRate * p1HoursWorked * 52;
             double p2AnnualSalary = p2HourlyRate * p2HoursWorked * 52;
+            // Checking if Person 1 income is higher than Person 2
             bool moreIncome = p1AnnualSalary > p2AnnualSalary;
+            // Printing out results to console; Using \n to elimate the need to write two Console.WriteLines
             Console.WriteLine("Annual salary of Person 1:\n$" + p1AnnualSalary);
             Console.WriteLine("Annual salary of Person 2:\n$" + p2AnnualSalary);
             Console.WriteLine("Does Person 1 make more money than Person 2?\n" + moreIncome);
