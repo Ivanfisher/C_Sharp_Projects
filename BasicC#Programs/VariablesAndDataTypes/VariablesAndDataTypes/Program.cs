90f164f19f84d5a07a9c23e3ab34355d099639c1
Ivan Fisher <87035839+Ivanfisher@users.noreply.github.com>
2021-09-08 14:26:43 -0400

    Changed File

--- BasicC#Programs/VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
+++ BasicC#Programs/VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -10,9 +10,19 @@ namespace VariablesAndDataTypes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your name?");
-            string yourName = Console.ReadLine();
-            Console.WriteLine("Hello, " + yourName + "!");
+            Console.WriteLine("Please enter any number.");
+            long multiplication = Convert.ToInt64(Console.ReadLine()) * 50;
+            Console.WriteLine(multiplication);
+            Console.WriteLine("Please enter any integer less than 2 million.");
+            int addittion = Convert.ToInt32(Console.ReadLine()) + 25;
+            Console.WriteLine(addittion);
+            Console.WriteLine("Please enter any integer less than 2 million.");
+            double division = Convert.ToDouble(Console.ReadLine()) / 12.5;
+            Console.WriteLine(division);
+            Console.WriteLine("Please enter any integer less than 2 million.");
+            int remainder = Convert.ToInt32(Console.ReadLine()) % 7;
+            Console.WriteLine(remainder);
+            Console.Read();
         }
     }
 }
