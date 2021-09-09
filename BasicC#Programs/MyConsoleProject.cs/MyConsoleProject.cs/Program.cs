95718482ed60106043a24042200ca8924b2e46bb
Ivan Fisher <87035839+Ivanfisher@users.noreply.github.com>
2021-09-09 14:28:18 -0400

    Added Folder

--- BasicC#Programs/MyConsoleProject.cs/MyConsoleProject.cs/Program.cs
+++ BasicC#Programs/MyConsoleProject.cs/MyConsoleProject.cs/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 
 namespace MyConsoleProject.cs
@@ -9,6 +10,15 @@ namespace MyConsoleProject.cs
         {
             Console.WriteLine("Welcome to Acme Accounting Systems");
             Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.WriteLine("This assignment wants me to use string concatenation and the stringbuilder function.");
+            string first = "I wrote ";
+            string second = "this sentence in ";
+            string third = "three different parts.";
+            Console.WriteLine(first + second + third);
+            StringBuilder sb = new StringBuilder();
+            sb.Append("My name is ");
+            sb.Append("Ivan");
+            Console.WriteLine(sb);
             Console.Read();
         }
     }
