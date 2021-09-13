ed7799a83bb392fe88e35b25234a310f8d97b22b
Ivan Fisher <87035839+Ivanfisher@users.noreply.github.com>
2021-09-13 13:25:20 -0400

    Added Folder

---
+++ BasicC#Programs/CallingMethods/CallingMethods/MathOperations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class MathOperations
+    {
+        public static int InputCubed(int int1)
+        {
+            int1 = Convert.ToInt32(Math.Pow(int1, 3));
+            return int1;
+        }
+
+        public static int InputQuotient(int int1)
+        {
+            int1 = int1 / 6;
+            return int1;
+        }
+
+        public static int InputRemainder(int int1)
+        {
+            int1 = int1 % 6;
+            return int1;
+        }
+    }
+    
+}
---
+++ BasicC#Programs/CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an integer to perform math operations on:");
+            int int1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(int1 + " cubed is " + MathOperations.InputCubed(int1));
+            if (MathOperations.InputRemainder(int1) == 0)
+            {
+                Console.WriteLine(int1 + " is divisible by 6. The quotient is " + MathOperations.InputQuotient(int1) + " and the remainder is " + MathOperations.InputRemainder(int1) + ".");
+            }
+            else
+            {
+                Console.WriteLine(int1 + " is not evenly divisible by six. The quotient is " + MathOperations.InputQuotient(int1) + " and the remainder is " + MathOperations.InputRemainder(int1) + ".");
+            }
+            Console.Read();
+        }
+    }
+}
