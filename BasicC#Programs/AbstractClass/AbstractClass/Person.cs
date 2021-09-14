97ff9ccf9893e9e9f3c6820bcbd552050420c09b
Ivan Fisher <87035839+Ivanfisher@users.noreply.github.com>
2021-09-14 13:28:58 -0400

    Added Folder

---
+++ BasicC#Programs/AbstractClass/AbstractClass/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    // Creating an abstract class
+    public abstract class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Creating a method that prints out full name
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+    }
+}
---
+++ BasicC#Programs/AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating new object "name" and initializing it with FirstName value "Sample" and LastName value "Student"
+            Employee name = new Employee() { FirstName = "Sample", LastName = "Student" };
+            // Calling SayName method. method prints out full name
+            name.SayName();
+            Console.Read();
+        }
+    }
+}
