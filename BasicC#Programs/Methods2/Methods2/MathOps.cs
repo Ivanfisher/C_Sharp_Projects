0c6115180e578590ff5a45c841d1e264a8d616c0
Ivan Fisher <87035839+Ivanfisher@users.noreply.github.com>
2021-09-14 09:46:58 -0400

    Added Folder

---
+++ BasicC#Programs/Methods2/Methods2/MathOps.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods2
+{
+    class MathOps
+    {
+        public static void Math1(int int1, int int2)
+        {
+            int1 = Convert.ToInt32(Math.Pow(int1, 4));
+            Console.WriteLine(int2);
+        }
+    }
+}
