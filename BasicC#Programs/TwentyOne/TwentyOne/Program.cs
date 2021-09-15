0e9d4b19235e609c9c07c53b6eb204de2b328fc9
Ivan Fisher <87035839+Ivanfisher@users.noreply.github.com>
2021-09-15 12:28:51 -0400

    Added Folder

---
+++ BasicC#Programs/TwentyOne/TwentyOne/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TwentyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Calling the Deck() function, which creates a deck of 52 cards, and assigning it to variable deck
+            Deck deck = new Deck();
+
+            // Reassingning the var deck with the Shuffle() function. (shuffles a deck of cards)
+            deck.Shuffle(3);
+
+            // Writing out the entire deck of cards to the console
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            Console.WriteLine(deck.Cards.Count);
+            Console.Read();
+        }
+
+    }
+}
