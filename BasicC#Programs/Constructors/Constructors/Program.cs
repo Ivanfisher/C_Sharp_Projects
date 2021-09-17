04a3d66859990b4699595c5750b1bb77905826cd
Ivan Fisher <87035839+Ivanfisher@users.noreply.github.com>
2021-09-17 10:17:00 -0400

    Added Folder

---
+++ BasicC#Programs/Constructors/Constructors/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating two new instances of class Person
+            Person person1 = new Person("Mark");
+            Person person2 = new Person("Landon", 28);
+            // Creating a constant string
+            const string name = "Ivan Fisher";
+            // Creating an int variable with "var"
+            var age = 24;
+            Console.WriteLine(person2);
+            Console.Read();
+        }
+    }
+}
