f3861e6c631042a9730f4c3452adc502bb8c6bc9
Ivan Fisher <87035839+Ivanfisher@users.noreply.github.com>
2021-10-05 14:29:29 -0400

    Added CodeFirstProject project

---
+++ CodeFirstProject/CodeFirstProject/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace CodeFirstProject.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CodeFirstProject.StudentContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "CodeFirstProject.StudentContext";
+        }
+
+        protected override void Seed(CodeFirstProject.StudentContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ CodeFirstProject/CodeFirstProject/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace CodeFirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new StudentContext())
+            {
+                Console.Write("Enter the first name of new student: ");
+                var firstName = Console.ReadLine();
+                Console.Write("Enter the last name of a new student: ");
+                var lastName = Console.ReadLine();
+               // Adding student first name and last name, id gets added automatically
+                var student = new Student { FirstName = firstName, LastName = lastName };
+                db.Students.Add(student);
+                db.SaveChanges();
+                // Displaying all students
+                Console.WriteLine("A list of all students: ");
+                foreach (var item in db.Students)
+                {
+                    Console.WriteLine(item.FirstName + " " + item.LastName);
+                }    
+            }
+        }
+    }
+    // Creating a student class
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
+    }
+    // Creating a derived context that allows us to query and save data from a database
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
