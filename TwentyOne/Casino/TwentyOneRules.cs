edbd0adf68e4a439204515a891b336a2880c9bea
Ivan Fisher <87035839+Ivanfisher@users.noreply.github.com>
2021-10-05 14:27:12 -0400

    Added TwentyOne Project

---
+++ TwentyOne/Casino/TwentyOneRules.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino.TwentyOne
+{
+    public class TwentyOneRules
+    {
+        // Creating a dicionary of values for the enum members "Face"
+        private static Dictionary<Face, int> _cardValues = new Dictionary<Face, int>()
+        {
+            [Face.Two] = 2,
+            [Face.Three] = 3,
+            [Face.Four] = 4,
+            [Face.Five] = 5,
+            [Face.Six] = 6,
+            [Face.Seven] = 7,
+            [Face.Eight] = 8,
+            [Face.Nine] = 9,
+            [Face.Ten] = 10,
+            [Face.Jack] = 10,
+            [Face.Queen] = 10,
+            [Face.King] = 10,
+            [Face.Ace] = 1
+        };
+
+        // Creating a function that gets all possible hand values, since the ace can either be of value 1 or 11
+        private static int[] GetAllPossibleHandValues(List<Card> Hand)
+        {
+            int aceCount = Hand.Count(x => x.Face == Face.Ace);
+            int[] result = new int[aceCount + 1];
+            int value = Hand.Sum(x => _cardValues[x.Face]);
+            result[0] = value;
+            if (result.Length == 1) return result;
+
+            for (int i = 1; i < result.Length; i++)
+            {
+                value += (i * 10);
+                result[i] = value;
+            }
+            return result;
+        }
+
+        // Creating a method that checks if argument given has blackjack (hand value is 21)
+        public static bool CheckForBlackJack(List<Card> Hand)
+        {
+            int[] possibleValues = GetAllPossibleHandValues(Hand);
+            int value = possibleValues.Max();
+            if (value == 21) return true;
+            else return false;
+        }
+
+        // Creating a method that checks if argument given has busted (hand value is greater than 21)
+        public static bool IsBusted(List<Card> Hand)
+        {
+            int value = GetAllPossibleHandValues(Hand).Min();
+            if (value > 21) return true;
+            else return false;
+        }
+
+        // Method checks if the dealer should stay (if hand value is greater than 16 and less than 22)
+        public static bool ShouldDealerStay(List<Card> Hand)
+        {
+            int[] possibleHandValues = GetAllPossibleHandValues(Hand);
+            foreach (int value in possibleHandValues)
+            {
+                if (value > 16 && value < 22) return true;
+             
+            }
+            return false;
+        }
+
+        // Method checks if player or dealer has a higher value
+        public static bool? CompareHands(List<Card> PlayerHand, List<Card> DealerHand)
+        {
+            int[] playerResults = GetAllPossibleHandValues(PlayerHand);
+            int[] dealerResults = GetAllPossibleHandValues(DealerHand);
+
+            int playerScore = playerResults.Where(x => x < 22).Max();
+            int dealerScore = dealerResults.Where(x => x < 22).Max();
+
+            if (playerScore > dealerScore) return true;
+            else if (playerScore < dealerScore) return false;
+            else return null;
+        }
+    }
+}
---
+++ TwentyOne/TwentyOne/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using Casino;
+using Casino.TwentyOne;
+
+namespace TwentyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name.");
+            string playerName = Console.ReadLine();
+            if (playerName.ToLower() == "admin")
+            {
+                List<ExceptionEntity> Exceptions = ReadExceptions();
+                foreach (var exception in Exceptions)
+                {
+                    Console.WriteLine("{0} | {1} | {2} | {3}", exception.Id, exception.ExceptionType, exception.ExceptionMessage, exception.TimeStamp);
+                }
+                return;
+            }
+
+
+            bool validAnswer = false;
+            int bank = 0;
+            while (!validAnswer)
+            {
+                Console.WriteLine("And how much money did you bring today?");
+                validAnswer = int.TryParse(Console.ReadLine(), out bank);
+                if (!validAnswer) Console.WriteLine("Please enter digits only, no decimals.");
+            }
+
+            Console.WriteLine("Hello, {0}. Would you like to join in a game of 21 right now?", playerName);
+            string answer = Console.ReadLine().ToLower();
+            // Determining if player wants to play
+            if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "yea")
+            {
+                // Creating an instance of Player
+                Player player = new Player(playerName, bank);
+                player.Id = Guid.NewGuid();
+                using (StreamWriter file = new StreamWriter(@"C:\Users\ivanf\Logs\log.txt", true))
+                {
+                    file.WriteLine(player.Id);
+                }
+                // Creating an instance of TwentyOneGame
+                Game game = new TwentyOneGame();
+                // Adding player to game
+                game += player;
+                player.isActivelyPlaying = true;
+                // Execute code inside while loop as long as isActivelyPlaying is true and balance is greater than 0
+                while (player.isActivelyPlaying && player.Balance > 0)
+                {
+                    try
+                    {
+                        game.Play();
+                    }
+                    catch(FraudException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        UpdateDbWithException(ex);
+                        Console.ReadLine();
+                        return;
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine("An error occurred. Please contact your System Administrator.");
+                        UpdateDbWithException(ex);
+                        Console.ReadLine();
+                        return;
+                    }
+                }
+                game -= player;
+                if (player.isActivelyPlaying && player.Balance <= 0)
+                {
+                    Console.WriteLine("Sorry, you cannot play without money..");
+                }
+                Console.WriteLine("Thank you for playing!");
+            }
+            Console.WriteLine("Feel free to look around the casino. Bye for now.");
+            Console.Read();
+        }
+        private static void UpdateDbWithException(Exception ex)
+        {
+            // The connection to the database
+            string connectionString = @"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = TwentyOneGame; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+            // The query string
+            string queryString = @"INSERT INTO Exceptions (ExceptionType, ExceptionMessage, TimeStamp) VALUES (@ExceptionType, @ExceptionMessage, @TimeStamp)";
+
+            //
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                // Creating sql query command
+                SqlCommand command = new SqlCommand(queryString, connection);
+                // Adding parameter data types
+                command.Parameters.Add("@ExceptionType", SqlDbType.VarChar);
+                command.Parameters.Add("@ExceptionMessage", SqlDbType.VarChar);
+                command.Parameters.Add("@TimeStamp", SqlDbType.DateTime);
+                // Adding parameter values
+                command.Parameters["@ExceptionType"].Value = ex.GetType().ToString();
+                command.Parameters["@ExceptionMessage"].Value = ex.Message;
+                command.Parameters["@TimeStamp"].Value = DateTime.Now;
+                // Opening connectin, executing command, and then closing connection
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+
+        private static List<ExceptionEntity> ReadExceptions()
+        {
+            string connectionString = @"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = TwentyOneGame; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+            string queryString = @"SELECT Id, ExceptionType, ExceptionMessage, TimeStamp FROM Exceptions";
+
+            List<ExceptionEntity> Exceptions = new List<ExceptionEntity>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    ExceptionEntity exception = new ExceptionEntity();
+                    exception.Id = Convert.ToInt32(reader["Id"]);
+                    exception.ExceptionType = reader["ExceptionType"].ToString();
+                    exception.ExceptionMessage = reader["ExceptionMessage"].ToString();
+                    exception.TimeStamp = Convert.ToDateTime(reader["TimeStamp"]);
+                    Exceptions.Add(exception);
+                }
+                connection.Close();
+            }
+            return Exceptions;
+        }
+    }
+}
